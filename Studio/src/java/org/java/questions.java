09e0df970c423ce5d610f8130816c2a6ae262d70
Joshua Buehrer <louisproductions179@gmail.com>
2023-12-21 20:07:01 -0600

    part one

--- Studio/src/java/org/java/questions.java
+++ Studio/src/java/org/java/questions.java
@@ -1,4 +1,28 @@
 package org.java;
 
-public class questions {
+import java.util.ArrayList;
+import java.util.Scanner;
+public abstract class questions {
+    private String field;
+    private int numQuestions;
+    private ArrayList<String> questions;
+    private ArrayList<String> userAnswers;
+    Scanner input = new Scanner(System.in);
+
+
+    public void addQuestion(String ){
+
+    }
+
+    public String askQuestion() {
+        System.out.println();
+    }
+
+
+
+
+    public String submitAnswer(String userAnswer) {
+
+    }
+
 }
